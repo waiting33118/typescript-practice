028f820cabba40bf7c15f5471ecff3f263245cc5
Tony Chung <waiting33118@gmail.com>
2021-04-04 20:51:02 +0800

    feat: day 1

---
+++ Basic/Day1/index.js
@@ -0,0 +1,6 @@
+"use strict";
+const message = 'Hello world';
+function say(something) {
+    console.log(something);
+}
+say(message);
---
+++ Basic/Day1/index.ts
@@ -0,0 +1,7 @@
+const message = 'Hello world'
+
+function say(something: string): void {
+  console.log(something.toUpperCase())
+}
+
+say(message)
